791b6ed9a82173b8e71efd9406a27b04f47974d9
kevin harris <kcharris77@gmail.com>
2024-04-02 02:58:50 -0500

    Update table to show values

--- PersonalDB/src/components/MainList.vue
+++ PersonalDB/src/components/MainList.vue
@@ -1,53 +1,102 @@
 <template>
-    <!-- <v-data-table-virtual
+    <v-data-table-virtual
         :headers="headers"
         :items="virtualItems"
         height="60dvh"
         item-value="name"
-    ></v-data-table-virtual> -->
-    <v_btn @click="get_list">Press me</v_btn>
-    <v_btn @click="do_thing">Press me</v_btn>
+    ></v-data-table-virtual>
+    <v-btn @click="get_list">Press me</v-btn>
+    <v-btn @click="do_thing">Press me</v-btn>
     <p>{{ res }} and more</p>
 </template>
 
 <script setup>
 import { invoke } from "@tauri-apps/api/tauri";
-import { ref } from "vue";
+import { ref, reactive, computed } from "vue";
+import { VDataTableVirtual, VBtn } from "vuetify/components";
 
     const id = ref(1);
     const res = ref("")
 
     async function get_list(){
-        res.value = await invoke("find_items_by_parent_id", {id: id.value})
-    }
+            res.value = await invoke("find_items_by_parent_id", {id: id.value})
+        }
     function do_thing(){
         res.value += "a"
-    }
-
-        // export default {
-        //     data () {
-        //         return {
-        //         headers: [
-        //             { title: 'Name', align: 'start', key: 'name' },
-        //             { title: 'Priority', align: 'end', key: 'priority' },
-        //             { title: 'Est Time', align: 'end', key: 'estTime' },
-        //             { title: 'Link', align: 'end', key: 'link' },
-        //             { title: 'Edit', align: 'end', key: 'edit' },
-        //             { titel: "Del", align: "end", key: "del"},
-        //         ]
-        //         }
-        // },
-
-        // // computed: {
-        // //     virtualItems () {
-        // //         let json_list = get_list()
+    } 
+    const virtualItems = computed(()=>{
+            let l = []
+            const item = {
+                    name: "name",
+                    priority: 0,
+                    estTime: Date(25),
+                    link: "https://www.google.com/",
+                    edit: "e but",
+                    del: "del but"
+                }
+            for (let i =0; i< 20; i++){
                 
-        // //     return json_list // a json list containing the information above, revieced from a call to the database using rust
-        // //     // and stored and sent through a struct.
-        // //     },
-        // // },
-        // // methods:{
-        // //     get_list
-        // // }
-        // }
+                    l.push({...item})
+                }
+            return l
+        }
+    )
+
+    // headers cannot be reactive, but if I wanted a val that would update virtual items when changed I would use reactive() instead of ref().
+    const headers = ref([
+            { title: 'Name', align: 'start', key: 'name' },
+            { title: 'Priority', align: 'end', key: 'priority' },
+            { title: 'Est Time', align: 'end', key: 'estTime' },
+            { title: 'Link', align: 'end', key: 'link' },
+            { title: 'Edit', align: 'end', key: 'edit' },
+            { titel: "Del", align: "end", key: "del"},
+    ])
+
+// export default {
+//     setup(){
+//         const id = ref(1);
+//         const res = ref("")
+//         return {id, res}
+//     },
+//     data () {
+//         return {
+//         headers: [
+//             { title: 'Name', align: 'start', key: 'name' },
+//             { title: 'Priority', align: 'end', key: 'priority' },
+//             { title: 'Est Time', align: 'end', key: 'estTime' },
+//             { title: 'Link', align: 'end', key: 'link' },
+//             { title: 'Edit', align: 'end', key: 'edit' },
+//             { titel: "Del", align: "end", key: "del"},
+//         ],
+//         }
+//     },
+
+//     computed: {
+//         virtualItems() {
+//             let l = []
+//             const item = {
+//                     name: "name",
+//                     priority: 0,
+//                     estTime: Date(25),
+//                     link: "https://www.google.com/",
+//                     edit: "e but",
+//                     del: "del but"
+//                 }
+//             for (let i =0; i< 20; i++){
+                
+//                     l.push({...item})
+//                 }
+//             return l
+//         },
+//     },
+//     methods:{
+//         get_list: async function(){
+//             res.value = await invoke("find_items_by_parent_id", {id: id.value})
+//         },
+//         do_thing: function(){
+//             res.value += "a"
+//         } 
+// }
+// }
 </script>
+
