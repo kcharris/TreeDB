a73a87aa8c0ae9ceac3a8d6ff7d3c88c89c21a4b
kevin harris <kcharris77@gmail.com>
2024-04-02 06:27:10 -0500

    Create CalendarField with name Prop
    Replaces text fields in add item.

---
+++ PersonalDB/src/components/CalendarField.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {ref} from "vue";
+    defineProps({
+        name: String
+    })
+    const dialog = ref(false)
+  
+</script>
+<template>
+    <div>
+        <v-text-field
+        :label="name"
+        hint="Use YYYY-MM-DD format"
+        prepend-inner-icon="mdi-calendar"
+        @click:prepend-inner="dialog = true"
+        >
+        </v-text-field>
+        <v-dialog
+            v-model="dialog"
+            max-width="500"
+            >
+            <v-card>
+                <v-date-picker></v-date-picker>
+            </v-card>
+            <template v-slot:actions>
+                <v-btn
+                class="ms-auto"
+                text="Ok"
+                @click="dialog = false"
+                ></v-btn>
+            </template>   
+        </v-dialog>
+    </div>
+</template>
