3510529463b393968ecf65a3c4b5f7f178d6759e
kevin harris <kcharris77@gmail.com>
2024-04-02 09:37:32 -0500

    Add function for emitted value

--- PersonalDB/src/App.vue
+++ PersonalDB/src/App.vue
@@ -11,6 +11,11 @@ import {ref} from "vue"
     const values = ref({
       v: {}
     })
+    function addItem(itemObject:object){
+      values.value.v = itemObject;
+      // send to rust.
+
+    }
 </script>
 
 <template>
@@ -21,44 +26,10 @@ import {ref} from "vue"
     <FullDetails/>
     <v-toolbar>
       <v-spacer/>
-      <CreateNewItemPopup @send-values="(v:object) => values.v = v"/>
-        <span>{{ values }}</span>
+      <CreateNewItemPopup @send-values="addItem"/>
+        <!-- <span>{{ values }}</span> -->
     </v-toolbar>
     <MainList />
-    <!-- <div class="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-        <p>
-          Recommended IDE setup:
-          <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-          +
-          <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-          +
-          <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-            >Tauri</a
-          >
-          +
-          <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-            >rust-analyzer</a
-          >
-        </p>
-
-        <Greet />
-    </div> -->
     </v-main>
   </v-app>
   
