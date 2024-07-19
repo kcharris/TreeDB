a5fc187b52c7e9f9ffe9c2f337a6f2506cd38099
kevin harris <20600822+kcharris@users.noreply.github.com>
2024-07-19 12:27:00 -0500

    Add unit test
    Also minor update to delete item fn

--- PersonalDB/src-tauri/src/db/item_db_util.rs
+++ PersonalDB/src-tauri/src/db/item_db_util.rs
@@ -6,7 +6,7 @@ use crate::errors::ItemDBError;
 use crate::migrator;
 use crate::entities::*;
 use sea_orm_migration::prelude::*;
-use sea_orm::{ActiveValue, ActiveModelTrait, EntityTrait, QueryFilter, ModelTrait, ColumnTrait, DatabaseConnection, Database, InsertResult};
+use sea_orm::{ActiveValue, ActiveModelTrait, EntityTrait, QueryFilter, ModelTrait, ColumnTrait, DatabaseConnection, Database, InsertResult, DeleteResult};
 use crate::db::db_util::*;
 use serde_json::json;
 
@@ -49,11 +49,11 @@ pub async fn add_item(db_name: String, payload: String) -> Result<Option<i32>, I
 }
 
 #[tauri::command]
-pub async fn delete_item(db_name: String, id: i32) -> Result<(), ItemDBError>{
+pub async fn delete_item(db_name: String, id: i32) -> Result<u64, ItemDBError>{
     let db = get_db_conn(&db_name).await?;
     let item1 = item::Entity::find_by_id(id).one(&db).await?.unwrap();
     let res = item1.delete(&db).await?;
-    Ok(())
+    Ok(res.rows_affected)
 }
 
 #[tauri::command]
@@ -67,4 +67,69 @@ pub async fn update_item(db_name: String, payload: String) -> Result<(), ItemDBE
     
     let _res = item.save(&db).await?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // test with "$ cargo test -- --test-threads=1 ", this is because of issues with async functions sharing resources
+    // Sets up a test database to avoid over-writing original and get the connection
+    pub async fn setup() -> Result<(), ItemDBError> {
+        delete_db_file("test_database");
+        create_db_file("test_database");
+        run_migrator("test_database").await?;
+
+        return Ok(());
+    }
+
+    #[async_std::test]
+    pub async fn test_async_item() -> Result<(), ItemDBError> {
+        setup().await?;
+        let db_name = "test_database".to_owned();
+
+        // test insert
+        let json_str = r#"{
+            "name": "tester1"
+        }"#.to_owned();
+
+        let res = add_item(db_name.clone(), json_str).await?;
+        assert_eq!(res, Some(1), "item insert failed");
+    
+
+        // test get
+        let payload = get_item_by_id(db_name.clone(), 1).await?;
+        println!("{payload}");
+        let json_item = serde_json::from_str(&payload).unwrap();
+
+        let mut item = item::ActiveModel{..Default::default()};
+        item.set_from_json(json_item)?;
+
+        assert_eq!(item.name, sea_orm::ActiveValue::Set("tester1".to_owned()), "item get failed");
+
+
+        // test update
+        let json_str = r#"{
+            "name": "updated1",
+            "id": 1
+        }"#.to_owned();
+
+        update_item(db_name.clone(), json_str).await?;
+
+        let payload = get_item_by_id(db_name.clone(), 1).await?;
+        let json_item = serde_json::from_str(&payload).unwrap();
+
+        let mut item = item::ActiveModel{..Default::default()};
+        item.set_from_json(json_item)?;
+
+        assert_eq!(item.name, sea_orm::ActiveValue::Set("updated1".to_owned()), "item update failed");
+        
+
+        // test delete
+        let db_name = "test_database".to_owned();
+        let res = delete_item(db_name, 1).await?;
+
+        assert_eq!(res, 1, "delete failed");
+        Ok(())
+    }
 }
