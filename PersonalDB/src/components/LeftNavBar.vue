51817de39346382dfedcdb05f555b9f26c20f8a3
kevin harris <20600822+kcharris@users.noreply.github.com>
2024-08-28 21:46:34 -0500

    Update LeftNavBar.vue buttons

--- PersonalDB/src/components/LeftNavBar.vue
+++ PersonalDB/src/components/LeftNavBar.vue
@@ -8,12 +8,13 @@
 <template>
     <v-navigation-drawer permanent width="135" location="left">
         <v-list-item @click="changePage(0)" title="List"></v-list-item>
-        <!-- <v-list-item link title="Tags"></v-list-item> -->
         <v-divider></v-divider>
-        <v-list-item @click="changePage(1)" title="DB Manager"></v-list-item>
+        <v-list-item @click="changePage(1)" title="Tags"></v-list-item>
         <v-divider></v-divider>
-        <v-list-item @click="changePage(2)" title="Backups"></v-list-item>
+        <v-list-item @click="changePage(2)" title="DB Manager"></v-list-item>
         <v-divider></v-divider>
-        <v-list-item @click="changePage(3)" title="Settings"></v-list-item>
+        <v-list-item @click="changePage(3)" title="Backups"></v-list-item>
+        <!-- <v-divider></v-divider>
+        <v-list-item @click="changePage(4)" title="Settings"></v-list-item> -->
     </v-navigation-drawer>
 </template>
