7ed51cc780944ff274e16a9e268c1585dc8af0b2
kevin harris <20600822+kcharris@users.noreply.github.com>
2024-08-31 01:37:50 -0500

    Update nav drawer to be smaller and expand on hover

--- PersonalDB/src/components/LeftNavBar.vue
+++ PersonalDB/src/components/LeftNavBar.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-    const emit = defineEmits(["updatePage"])
 
+    const emit = defineEmits(["updatePage"])
     function changePage(pageNum:number){
         emit("updatePage", pageNum)
     }
 </script>
 <template>
-    <v-navigation-drawer permanent width="135" location="left">
-        <v-list-item @click="changePage(0)" title="List"></v-list-item>
+    <v-navigation-drawer expand-on-hover rail location="left">
+        <v-list-item prepend-icon="mdi-list-box" @click="changePage(0)" title="List"></v-list-item>
         <v-divider></v-divider>
-        <v-list-item @click="changePage(1)" title="Tags"></v-list-item>
+        <v-list-item prepend-icon="mdi-tag" @click="changePage(1)" title="Tags"></v-list-item>
         <v-divider></v-divider>
-        <v-list-item @click="changePage(2)" title="DB Manager"></v-list-item>
+        <v-list-item prepend-icon="mdi-database" @click="changePage(2)" title="DB Manager"></v-list-item>
         <v-divider></v-divider>
-        <v-list-item @click="changePage(3)" title="Backups"></v-list-item>
+        <v-list-item prepend-icon="mdi-backup-restore" @click="changePage(3)" title="Backups"></v-list-item>
         <!-- <v-divider></v-divider>
         <v-list-item @click="changePage(4)" title="Settings"></v-list-item> -->
     </v-navigation-drawer>
